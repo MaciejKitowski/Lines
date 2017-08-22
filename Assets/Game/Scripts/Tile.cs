83e364c487fddb929b1ab9cb81b828073388b305
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-08-22 17:59:01 +0200

    Tile initial

---
+++ Assets/Game/Scripts/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Tile : MonoBehaviour {
+    /*private NavMeshAgent navMesh;
+
+	void Start () {
+        navMesh = GetComponent<NavMeshAgent>();
+
+
+        NavMeshPath path = new NavMeshPath();
+        Vector3 pos = new Vector3(-2.25f, 0, 0);
+        navMesh.CalculatePath(pos, path);
+
+        Debug.Log(path.status);
+        navMesh.SetPath(path);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = new Quaternion(0, 0, 0, 0);
+    }*/
+}
