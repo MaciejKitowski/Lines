a81e30824ffbd30fd31eff5e94e9c44bead9165f
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-08-22 13:41:16 +0200

    ArenaEditor - better logging

--- Assets/Game/Editor/ArenaEditor.cs
+++ Assets/Game/Editor/ArenaEditor.cs
@@ -6,8 +6,11 @@ using UnityEditor;
 
 [CustomEditor(typeof(Arena))]
 public class ArenaEditor : Editor {
+    public enum logMode { FULL, SUMMARY_ONLY, NONE }
+
     private bool displayOffMeshLinkButtons = false;
     private float buttonSpace = 20f;
+    private logMode logmode = logMode.FULL;
     
 
     void OnEnable() {
@@ -17,6 +20,7 @@ public class ArenaEditor : Editor {
     public override void OnInspectorGUI() {
         serializedObject.Update();
 
+        logmode = (logMode)EditorGUILayout.EnumPopup("Logging mode", logmode);
         displayOffMeshLinkButtons = EditorGUILayout.Foldout(displayOffMeshLinkButtons, "Off Mesh Links");
 
         if (displayOffMeshLinkButtons) displayOffMeshLinksButtons();
@@ -33,17 +37,20 @@ public class ArenaEditor : Editor {
     }
 
     private void clearOffMeshLinks() {
+        if (logmode == logMode.FULL) Debug.Log("Clear all Off Mesh Links.");
+
         int counter = 0;
 
         foreach(var tile in getTiles()) {
             OffMeshLink link = tile.GetComponent<OffMeshLink>();
             if(link != null) {
+                if (logmode == logMode.FULL) Debug.Log(string.Format("Clear [{0},{1}] off mesh link.", tile.name, tile.transform.parent.name), tile);
                 DestroyImmediate(link);
                 ++counter;
             }
         }
 
-        Debug.Log(string.Format("Removed {0} Off Mesh Links.", counter));
+        if (logmode != logMode.NONE) Debug.Log(string.Format("Removed {0} Off Mesh Links.", counter));
     }
 
     private void generateOffMeshLinks() {
@@ -51,6 +58,8 @@ public class ArenaEditor : Editor {
     }
 
     private GameObject[,] getTiles() {
+        if (logmode == logMode.FULL) Debug.Log("Get arena tiles.");
+
         GameObject obj = ((Arena)target).gameObject;
         int maxX = obj.transform.GetChild(0).childCount;
         int maxY = obj.transform.childCount;
@@ -59,11 +68,11 @@ public class ArenaEditor : Editor {
         for(int y = 0; y < maxY; ++y) {
             for(int x = 0; x < maxX; ++x) {
                 tiles[x, y] = obj.transform.GetChild(y).GetChild(x).gameObject;
-                Debug.Log(string.Format("[{0},{1}] - {2}", x, y, tiles[x, y].name), tiles[x, y]);
+                if (logmode == logMode.FULL) Debug.Log(string.Format("Loaded tile: [{0},{1}] - {2}", x, y, tiles[x, y].name), tiles[x, y]);
             }
         }
 
-        Debug.Log(string.Format("Loaded {0} tiles.", tiles.Length));
+        if (logmode != logMode.NONE) Debug.Log(string.Format("Loaded {0} tiles.", tiles.Length));
 
         return tiles;
     }
