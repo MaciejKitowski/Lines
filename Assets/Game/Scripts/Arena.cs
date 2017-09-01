68245e96475409264e1e1b63cd3d938cd7f5845d
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-09-01 12:05:06 +0200

    Load tiles to array

--- Assets/Game/Scripts/Arena.cs
+++ Assets/Game/Scripts/Arena.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Arena : MonoBehaviour {
+    private ArenaTile[,] _tile;
+    
+    public ArenaTile[,] tile { get { return _tile; } private set { _tile = value; } }
 
-	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+        int maxX = transform.GetChild(0).childCount;
+        int maxY = transform.childCount;
+        tile = new ArenaTile[maxX, maxY];
+
+        for(int y = 0; y < maxY; ++y) {
+            for(int x = 0; x < maxX; ++x) {
+                tile[x, y] = transform.GetChild(y).GetChild(x).GetComponent<ArenaTile>();
+            }
+        }
 	}
 }
