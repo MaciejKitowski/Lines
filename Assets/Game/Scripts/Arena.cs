55e7052ae9b6453fb886263e555c707db2fa0961
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-09-02 11:36:13 +0200

    Check rows for points

--- Assets/Game/Scripts/Arena.cs
+++ Assets/Game/Scripts/Arena.cs
@@ -2,14 +2,16 @@
 using UnityEngine;
 
 public class Arena : MonoBehaviour {
+    [SerializeField] private int tilesInLineToRemove = 3;
     private ArenaTile[,] _tile;
     private List<ArenaTile> tileList;   //Used to found empty tiles
+    private int maxX, maxY;
     
     public ArenaTile[,] tile { get { return _tile; } private set { _tile = value; } }
 
 	void Start () {
-        int maxX = transform.GetChild(0).childCount;
-        int maxY = transform.childCount;
+        maxX = transform.GetChild(0).childCount;
+        maxY = transform.childCount;
         tile = new ArenaTile[maxX, maxY];
         tileList = new List<ArenaTile>();
 
@@ -21,9 +23,33 @@ public class Arena : MonoBehaviour {
         }
 	}
 
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.C)) checkPoints();
+    }
+
     public List<ArenaTile> getEmptyTiles() {
         Debug.Log(string.Format("Found {0} empty tiles.", tileList.FindAll(b => b.empty).Count));
 
         return tileList.FindAll(b => b.empty);
+    }
+
+    public void checkPoints() {
+        //Check rows
+        for(int y = 0; y < maxY; ++y) {
+            int sameColor = 1;
+            for(int x = 1; x < maxX; ++x) {
+                if(!tile[x, y].empty && !tile[x - 1, y].empty) {
+                    if (tile[x, y].tile.color == tile[x - 1, y].tile.color) {
+                        ++sameColor;
+                        if (sameColor == tilesInLineToRemove) {
+                            Debug.Log(string.Format("Remove row {0}", y + 1));
+                        }
+                    }
+                    else sameColor = 1;
+                }
+            }
+        }
+
+        //Check columns
     }
 }
