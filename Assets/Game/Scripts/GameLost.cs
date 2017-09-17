57081748efdae23bc3df99ffafeb262956689584
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-09-17 15:19:19 +0200

    Flickering high score text

--- Assets/Game/Scripts/GameLost.cs
+++ Assets/Game/Scripts/GameLost.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameLost : MonoBehaviour {
+    [SerializeField] private GameObject highScoreText;
+    [SerializeField] private float highScoreFlickerDelay = 0.5f;
 
-	// Use this for initialization
 	void Start () {
-		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
 	}
+
+    private IEnumerator newHighScore() {
+        while(this.isActiveAndEnabled) {
+            highScoreText.SetActive(!highScoreText.activeInHierarchy);
+            yield return new WaitForSeconds(highScoreFlickerDelay);
+        }
+    }
 }
